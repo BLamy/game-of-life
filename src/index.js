6710e159dd1104837c45893def203015a7735f49
Brett Lamy <bel423@me.com>
2017-11-01 22:05:35 -0400

    added styled-components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+html, body, #root {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    position: relative;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;        
+    background-color: #f2f2f2;
+}
+`
 
 ReactDOM.render(<App />, document.getElementById('root'));
