4a1028838bda53bb929ea83c5435e958f848911a
Brett Lamy <bel423@me.com>
2017-11-01 22:33:52 -0400

    cleaned up style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { evolveGrid } from "./lib/game-of-life"
 
+const Scaffold = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
 const Table = styled.table`
   border: 1px solid #E0E0E0;
   border-collapse: collapse;
-  width: 100%;
-  height: 100%;
+  width: calc(100% - 360px);
+  height: calc(100% - 40px);
+  margin: 20px; 
+  border-radius: 4px;
+  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+                    0 1px 10px 0 rgba(0, 0, 0, 0.12),
+                    0 2px 4px -1px rgba(0, 0, 0, 0.4);
 `;
 const Tr = styled.tr`
   border: 1px solid #E0E0E0;
@@ -18,6 +28,16 @@ const Td = styled.td`
   background-color: ${props => (props.alive ? "#607D8B" : "white")};
 `;
 
+const Sidebar = styled.div`
+  width: 320px;
+  height: 100%;
+  > h1 {
+    font-size: 18px;
+    width: 100%;
+    text-align: center;
+  }
+`
+
 // Taken from https://github.com/ramda/ramda/blob/v0.25.0/source/range.js
 const range = (from, to) => {
   var result = [];
@@ -28,7 +48,6 @@ const range = (from, to) => {
   }
   return result;
 }
-
 
 export default class extends Component {
   
@@ -63,6 +82,7 @@ export default class extends Component {
   render() {
     const { numberOfColumns = 10, numberOfRows = 10 } = this.props;
     return (
+      <Scaffold>
         <Table>
           {range(0, numberOfRows).map(row => (
             <Tr>
@@ -77,6 +97,8 @@ export default class extends Component {
             </Tr>
           ))}
         </Table>
+        <Sidebar><h1>Conway's Game of Life</h1></Sidebar>
+      </Scaffold>
     );
   }
 }
